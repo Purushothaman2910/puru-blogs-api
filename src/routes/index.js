34091cd047bae504db8a96d5d9007e1a8a2d0f97
Purushothaman2910 <velusamypurushothaman@gmail.com>
2024-12-18 11:08:08 +0530

    created index fils

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import BlogRoutes from './Blog.routes.js'
+import UserRoutes from './User.routes.js'
+import HealthCheckRoutes from './HealthCheck.routes.js'
+
+export {
+    BlogRoutes ,
+    UserRoutes ,
+    HealthCheckRoutes
+}
